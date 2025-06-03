cbc7e1b2ab9116be939e4d16731c298fa3b763de
Alexander-Linkov <fill.freeman@gmail.com>
2025-06-03 07:32:31 +0300

    Add OLED support (MWP)

--- keymap.c
+++ keymap.c
@@ -604,3 +604,70 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [4] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),  ENCODER_CCW_CW(KC_MPRV, KC_MNXT)  }
 };
 #endif
+
+#ifdef OLED_ENABLE
+static void render_status(void) {
+    switch (get_highest_layer(layer_state|default_layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("ENG\n"), false);
+            break;
+        case _JCUKEN:
+            oled_write_P(PSTR("RUS\n"), false);
+            break;
+        case _LOWER:
+            oled_write_P(PSTR("Lower\n"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("Raise\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("Adjust\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM:ON\n") : PSTR("NUM:OFF\n"), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAPS:ON\n") : PSTR("CAPS:OFF\n"), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR:ON\n") : PSTR("SCR:OFF\n"), false);
+
+    switch (get_unicode_input_mode()) {
+        case UNICODE_MODE_WINCOMPOSE:
+            oled_write_P(PSTR("WIN\n"), false);
+            break;
+        case UNICODE_MODE_MACOS:
+            oled_write_P(PSTR("MacOS\n"), false);
+            break;       
+        case UNICODE_MODE_LINUX:
+            oled_write_P(PSTR("Linux\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undef OS\n"), false);
+    }
+}
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        render_status();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+    } else {
+        render_logo();  // Renders a static logo
+        oled_scroll_left();  // Turns on scrolling
+    }
+    
+    return false;
+}
+#endif
