7d853cc1d018502d60495641fe4cd07ea1d048b8
Alexander-Linkov <fill.freeman@gmail.com>
2025-07-20 07:48:02 +0300

    Fix toggle

--- keymap.c
+++ keymap.c
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_JCUKEN,          KC_1, KC_2,    KC_3,    KC_4,          KC_5,                                         KC_6,            KC_7,          KC_8,    KC_9,    KC_0,    TG(1),
+  KC_JCUKEN,          KC_1, KC_2,    KC_3,    KC_4,          KC_5,                                         KC_6,            KC_7,          KC_8,    KC_9,    KC_0,    TG(2),
   KC_RBRC,            KC_Q, KC_W,    KC_E,    KC_R,          KC_T,                                         KC_Y,            KC_U,          KC_I,    KC_O,    KC_P,    KC_LBRC,
   KC_GRV,             KC_A, KC_S,    KC_D,    KC_F,          KC_G,                                         KC_H,            KC_J,          KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   LALT_T(KC_BSLS),    KC_Z, KC_X,    KC_C,    KC_V,          KC_B,           KC_MUTE,       KC_MPLY,       KC_N,            KC_M,          KC_COMM, KC_DOT,  KC_SLSH, RALT_T(KC_MINS),
@@ -194,7 +194,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_8,
     KC_9,
     KC_0,
-    TG(1),
+    TG(2),
     UP(RU_HARD_SIGN_L, RU_HARD_SIGN_U), //second row
     UP(RU_YI_L, RU_YI_U),
     UP(RU_TS_L, RU_TS_U),
